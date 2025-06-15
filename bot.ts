2880bcea2573aff21c7359332c296061ae65401f
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-06-15 20:44:02 +0300

    feat: remove not worked listener, fix import, add todo

--- bot.ts
+++ bot.ts
@@ -6,7 +6,10 @@ import { Bot } from "grammy";
 const bot = new Bot(process.env.BOT_TOKEN ?? ""); // <-- place your bot token in this string
 
 // Register listeners to handle messages
-bot.on("message:text", (ctx) => ctx.reply("Echo: " + ctx.message.text));
-bot.on("message:text");
+bot.on("message:text", (ctx) => {
+//   console.log(ctx);
+  ctx.reply("Echo: " + ctx.message.text);
+});
+
 // Start the bot (using long polling)
 bot.start();
