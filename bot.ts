906b11c581d408dc608c4bab4e7f30998bcd225c
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-06-16 22:02:19 +0300

    feat: process command /start, listen :chat_shared service message

--- bot.ts
+++ bot.ts
@@ -1,15 +1,26 @@
 #!/usr/bin/env ts-node
 
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard, Keyboard } from "grammy";
 
 const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("BOT_TOKEN is unset");
 
 const bot = new Bot(token);
 
-bot.on("message:text", (ctx) => {
-//   console.log(ctx);
-  ctx.reply("Echo: " + ctx.message.text);
+const keyboard = new Keyboard().requestChat("Choose chat", 111).oneTime();
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello!", {
+    reply_markup: keyboard,
+  });
+});
+
+bot.on("message:chat_shared", (ctx) => {
+  //   console.log(ctx);
+  if (ctx.update.message) {
+    const chatId = ctx.update.message.chat_shared.chat_id;
+    console.log({ chatId });
+  }
 });
 
 bot.start();
